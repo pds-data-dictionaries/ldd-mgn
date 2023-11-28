48e2af2b7afb941be3a6479f9fea3ed0680f0551
PDSEN Ops <pds-ops@jpl.nasa.gov>
2023-11-28 14:30:13 -0800

    Apply latest LDD Doc Gen Config updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Magellan Mission Dictionary'
+project = 'Example Namespace'
 # copyright = overidden by CSS specified below
 author = 'NASA Planetary Data System'
 
